9977a5ad55d24991f84d916100b89727c1f22817
Andrey <Andr3y-Kirill@yandex.ru>
2021-04-16 17:31:21 +0300

    Registration, db, some models added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, make_response, jsonify
+from flask import Flask, make_response, jsonify, render_template
+from werkzeug.utils import redirect
+from data.models import User
+from data.forms import RegisterForm
 from data import db_session
 
 app = Flask(__name__)
@@ -10,8 +13,37 @@ def not_found(_):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 
+@app.route('/')
+def index():
+    db_sess = db_session.create_session()
+    return render_template('main.html')
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('registration.html', title='Registration', form=form,
+                                   message="Passwords don't match")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('registration.html', title='Registration', form=form,
+                                   message="This user already exists")
+        user = User(
+            name=form.name.data,
+            surname=form.surname.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/')
+    return render_template('registration.html', title='Регистрация', form=form)
+
+
 def main():
-    db_session.global_init("")
+    db_session.global_init("db/example.db")
     app.run()
 
 
